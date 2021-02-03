cbbe240b16b661f2aa59de27823c747b3507defc
siyeon Kim <siyeonk27@gmail.com>
2021-02-03 13:01:00 +0900

    url ,]

--- piroproject/urls.py
+++ piroproject/urls.py
@@ -20,7 +20,8 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('community/', include('blog.urls'))
+    path('community/', include('blog.urls')),
     path('accounts/', include('accounts.urls')),
+]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
