9a27e813768cc3eb52101c32afe3a792970c5f5d
siyeon Kim <siyeonk27@gmail.com>
2021-02-16 12:02:50 +0900

    faq json file

--- faq/admin.py
+++ faq/admin.py
@@ -1,7 +1,1 @@
-from django.contrib import admin
-from faq.models import Qna
-
-@admin.register(Qna)
-class QnaAdmin(admin.ModelAdmin):
-    list_display = ('question', 'answer')
 
--- faq/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class Qna(models.Model):
-    question = models.TextField(default=None, verbose_name='질문')
-    answer = models.TextField(default=None, verbose_name='답변')
-
